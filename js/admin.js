b4af97ca75caf5a0d9e4b8035d2c93cf7043f7f0
Gia Bao <giabaotran912@gmail.com>
2021-12-30 09:04:01 +0700

    update admin page

--- js/admin.js
+++ js/admin.js
@@ -1,15 +1,20 @@
-document.getElementById("welcome").textContent =
-  "Hello " + accountObject.fullname;
+// document.getElementById("welcome").textContent =
+//   "Hello " + accountObject.fullname;
 
 const logOut = () => {
   sessionStorage.removeItem("ACCOUNT");
   window.location.href = "../Page/login.html";
 };
 
-const test = function newFunc() {
-  console.log(123);
-};
-
-newFunc();
-console.log(typeof test);
-console.log(typeof logOut);
+const changeContent = (id, tabId) => {
+  let listContent = document.getElementsByClassName('body__right__content');
+  let listTab = document.getElementsByClassName('body__left__item');
+  for (let i = 0; i < listContent.length; i++) {
+    listContent[i].classList.remove('active');
+  }
+  for (let i = 0; i < listTab.length; i++) {
+    listTab[i].classList.remove('active');
+  }
+  document.getElementById(id).classList.add('active');
+  document.getElementById(tabId).classList.add('active');
+}
